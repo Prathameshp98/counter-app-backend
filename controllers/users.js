de9e9ccf048d346b60992d93a30ca139882faf90
Prathamesh Patil <prathameshpatil2812@gmail.com>
2024-07-02 00:00:13 +0530

    added some data for more variety

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,14 @@
 
 const User = require('../models/User');
 const usersData = require('../utils/usersData');
+const newData = require('../data/data');
 
 exports.getUsers = (req, res, next) => {
+
+    const name = req.query.name;
+    const startDate = req.query.startdate;
+    const endDate = req.query.enddate;
+
     User.find()
         .then(users => {
             const dt = usersData(users[0].data.AuthorWorklog);
@@ -10,7 +16,30 @@ exports.getUsers = (req, res, next) => {
             res.status(200).json(users);
         })
         .catch((err) => {
-            console.log(err);
             res.status(500).json({data: err});
         });
 };
+
+exports.addOne = (req, res, next) => {
+
+    User.find()
+        .then(users => {
+            users[0].data.AuthorWorklog.rows.push(newData);
+            console.log(users[0]);
+
+            const updatedUsers = new User(users[0]);
+
+            updatedUsers.save()
+                .then(users => {
+                    res.status(201).json({'data': users});
+                })
+                .catch((err) => {
+                    console.log(err);
+                    res.status(500).json({data: err});
+                });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({data: err});
+        });
+}
---
+++ data/data.js
@@ -0,0 +1,634 @@
+const newData = {
+    "name": "narendra@dev.ai",
+    "totalActivity": [
+      {
+        "name": "PR Open",
+        "value": "10"
+      },
+      {
+        "name": "PR Merged",
+        "value": "7"
+      },
+      {
+        "name": "Commits",
+        "value": "24"
+      },
+      {
+        "name": "PR Reviewed",
+        "value": "1"
+      },
+      {
+        "name": "PR Comments",
+        "value": "2"
+      },
+      {
+        "name": "Incident Alerts",
+        "value": "1"
+      },
+      {
+        "name": "Incidents Resolved",
+        "value": "1"
+      }
+    ],
+    "dayWiseActivity": [
+      {
+        "date": "2024-05-06",
+        "items": {
+          "children": [
+            {
+              "count": "2",
+              "label": "PR Open",
+              "fillColor": "#EF6B6B"
+            },
+            {
+              "count": "1",
+              "label": "PR Merged",
+              "fillColor": "#61CDBB"
+            },
+            {
+              "count": "3",
+              "label": "Commits",
+              "fillColor": "#FAC76E"
+            },
+            {
+              "count": "1",
+              "label": "PR Reviewed",
+              "fillColor": "#C2528B"
+            },
+            {
+              "count": "1",
+              "label": "PR Comments",
+              "fillColor": "#0396A6"
+            },
+            {
+              "count": "0",
+              "label": "Incident Alerts",
+              "fillColor": "#5F50A9"
+            },
+            {
+              "count": "0",
+              "label": "Incidents Resolved",
+              "fillColor": "#8F3519"
+            }
+          ]
+        }
+      },
+      {
+        "date": "2024-05-07",
+        "items": {
+          "children": [
+            {
+              "count": "1",
+              "label": "PR Open",
+              "fillColor": "#EF6B6B"
+            },
+            {
+              "count": "2",
+              "label": "PR Merged",
+              "fillColor": "#61CDBB"
+            },
+            {
+              "count": "2",
+              "label": "Commits",
+              "fillColor": "#FAC76E"
+            },
+            {
+              "count": "0",
+              "label": "PR Reviewed",
+              "fillColor": "#C2528B"
+            },
+            {
+              "count": "0",
+              "label": "PR Comments",
+              "fillColor": "#0396A6"
+            },
+            {
+              "count": "1",
+              "label": "Incident Alerts",
+              "fillColor": "#5F50A9"
+            },
+            {
+              "count": "1",
+              "label": "Incidents Resolved",
+              "fillColor": "#8F3519"
+            }
+          ]
+        }
+      },
+      {
+        "date": "2024-05-08",
+        "items": {
+          "children": [
+            {
+              "count": "2",
+              "label": "PR Open",
+              "fillColor": "#EF6B6B"
+            },
+            {
+              "count": "1",
+              "label": "PR Merged",
+              "fillColor": "#61CDBB"
+            },
+            {
+              "count": "4",
+              "label": "Commits",
+              "fillColor": "#FAC76E"
+            },
+            {
+              "count": "0",
+              "label": "PR Reviewed",
+              "fillColor": "#C2528B"
+            },
+            {
+              "count": "1",
+              "label": "PR Comments",
+              "fillColor": "#0396A6"
+            },
+            {
+              "count": "0",
+              "label": "Incident Alerts",
+              "fillColor": "#5F50A9"
+            },
+            {
+              "count": "0",
+              "label": "Incidents Resolved",
+              "fillColor": "#8F3519"
+            }
+          ]
+        }
+      },
+      {
+        "date": "2024-05-09",
+        "items": {
+          "children": [
+            {
+              "count": "1",
+              "label": "PR Open",
+              "fillColor": "#EF6B6B"
+            },
+            {
+              "count": "1",
+              "label": "PR Merged",
+              "fillColor": "#61CDBB"
+            },
+            {
+              "count": "1",
+              "label": "Commits",
+              "fillColor": "#FAC76E"
+            },
+            {
+              "count": "0",
+              "label": "PR Reviewed",
+              "fillColor": "#C2528B"
+            },
+            {
+              "count": "0",
+              "label": "PR Comments",
+              "fillColor": "#0396A6"
+            },
+            {
+              "count": "0",
+              "label": "Incident Alerts",
+              "fillColor": "#5F50A9"
+            },
+            {
+              "count": "0",
+              "label": "Incidents Resolved",
+              "fillColor": "#8F3519"
+            }
+          ]
+        }
+      },
+      {
+        "date": "2024-05-10",
+        "items": {
+          "children": [
+            {
+              "count": "2",
+              "label": "PR Open",
+              "fillColor": "#EF6B6B"
+            },
+            {
+              "count": "0",
+              "label": "PR Merged",
+              "fillColor": "#61CDBB"
+            },
+            {
+              "count": "4",
+              "label": "Commits",
+              "fillColor": "#FAC76E"
+            },
+            {
+              "count": "0",
+              "label": "PR Reviewed",
+              "fillColor": "#C2528B"
+            },
+            {
+              "count": "0",
+              "label": "PR Comments",
+              "fillColor": "#0396A6"
+            },
+            {
+              "count": "0",
+              "label": "Incident Alerts",
+              "fillColor": "#5F50A9"
+            },
+            {
+              "count": "0",
+              "label": "Incidents Resolved",
+              "fillColor": "#8F3519"
+            }
+          ]
+        }
+      },
+      {
+        "date": "2024-05-11",
+        "items": {
+          "children": [
+            {
+              "count": "0",
+              "label": "PR Open",
+              "fillColor": "#EF6B6B"
+            },
+            {
+              "count": "1",
+              "label": "PR Merged",
+              "fillColor": "#61CDBB"
+            },
+            {
+              "count": "0",
+              "label": "Commits",
+              "fillColor": "#FAC76E"
+            },
+            {
+              "count": "0",
+              "label": "PR Reviewed",
+              "fillColor": "#C2528B"
+            },
+            {
+              "count": "0",
+              "label": "PR Comments",
+              "fillColor": "#0396A6"
+            },
+            {
+              "count": "0",
+              "label": "Incident Alerts",
+              "fillColor": "#5F50A9"
+            },
+            {
+              "count": "0",
+              "label": "Incidents Resolved",
+              "fillColor": "#8F3519"
+            }
+          ]
+        }
+      },
+      {
+        "date": "2024-05-12",
+        "items": {
+          "children": [
+            {
+              "count": "0",
+              "label": "PR Open",
+              "fillColor": "#EF6B6B"
+            },
+            {
+              "count": "0",
+              "label": "PR Merged",
+              "fillColor": "#61CDBB"
+            },
+            {
+              "count": "3",
+              "label": "Commits",
+              "fillColor": "#FAC76E"
+            },
+            {
+              "count": "0",
+              "label": "PR Reviewed",
+              "fillColor": "#C2528B"
+            },
+            {
+              "count": "0",
+              "label": "PR Comments",
+              "fillColor": "#0396A6"
+            },
+            {
+              "count": "0",
+              "label": "Incident Alerts",
+              "fillColor": "#5F50A9"
+            },
+            {
+              "count": "0",
+              "label": "Incidents Resolved",
+              "fillColor": "#8F3519"
+            }
+          ]
+        }
+      },
+      {
+        "date": "2024-05-13",
+        "items": {
+          "children": [
+            {
+              "count": "1",
+              "label": "PR Open",
+              "fillColor": "#EF6B6B"
+            },
+            {
+              "count": "0",
+              "label": "PR Merged",
+              "fillColor": "#61CDBB"
+            },
+            {
+              "count": "3",
+              "label": "Commits",
+              "fillColor": "#FAC76E"
+            },
+            {
+              "count": "0",
+              "label": "PR Reviewed",
+              "fillColor": "#C2528B"
+            },
+            {
+              "count": "0",
+              "label": "PR Comments",
+              "fillColor": "#0396A6"
+            },
+            {
+              "count": "1",
+              "label": "Incident Alerts",
+              "fillColor": "#5F50A9"
+            },
+            {
+              "count": "0",
+              "label": "Incidents Resolved",
+              "fillColor": "#8F3519"
+            }
+          ]
+        }
+      },
+      {
+        "date": "2024-05-14",
+        "items": {
+          "children": [
+            {
+              "count": "0",
+              "label": "PR Open",
+              "fillColor": "#EF6B6B"
+            },
+            {
+              "count": "2",
+              "label": "PR Merged",
+              "fillColor": "#61CDBB"
+            },
+            {
+              "count": "4",
+              "label": "Commits",
+              "fillColor": "#FAC76E"
+            },
+            {
+              "count": "0",
+              "label": "PR Reviewed",
+              "fillColor": "#C2528B"
+            },
+            {
+              "count": "0",
+              "label": "PR Comments",
+              "fillColor": "#0396A6"
+            },
+            {
+              "count": "0",
+              "label": "Incident Alerts",
+              "fillColor": "#5F50A9"
+            },
+            {
+              "count": "1",
+              "label": "Incidents Resolved",
+              "fillColor": "#8F3519"
+            }
+          ]
+        }
+      },
+      {
+        "date": "2024-05-15",
+        "items": {
+          "children": [
+            {
+              "count": "1",
+              "label": "PR Open",
+              "fillColor": "#EF6B6B"
+            },
+            {
+              "count": "1",
+              "label": "PR Merged",
+              "fillColor": "#61CDBB"
+            },
+            {
+              "count": "2",
+              "label": "Commits",
+              "fillColor": "#FAC76E"
+            },
+            {
+              "count": "0",
+              "label": "PR Reviewed",
+              "fillColor": "#C2528B"
+            },
+            {
+              "count": "0",
+              "label": "PR Comments",
+              "fillColor": "#0396A6"
+            },
+            {
+              "count": "0",
+              "label": "Incident Alerts",
+              "fillColor": "#5F50A9"
+            },
+            {
+              "count": "0",
+              "label": "Incidents Resolved",
+              "fillColor": "#8F3519"
+            }
+          ]
+        }
+      },
+      {
+        "date": "2024-05-16",
+        "items": {
+          "children": [
+            {
+              "count": "0",
+              "label": "PR Open",
+              "fillColor": "#EF6B6B"
+            },
+            {
+              "count": "1",
+              "label": "PR Merged",
+              "fillColor": "#61CDBB"
+            },
+            {
+              "count": "2",
+              "label": "Commits",
+              "fillColor": "#FAC76E"
+            },
+            {
+              "count": "0",
+              "label": "PR Reviewed",
+              "fillColor": "#C2528B"
+            },
+            {
+              "count": "1",
+              "label": "PR Comments",
+              "fillColor": "#0396A6"
+            },
+            {
+              "count": "0",
+              "label": "Incident Alerts",
+              "fillColor": "#5F50A9"
+            },
+            {
+              "count": "1",
+              "label": "Incidents Resolved",
+              "fillColor": "#8F3519"
+            }
+          ]
+        }
+      },
+      {
+        "date": "2024-05-17",
+        "items": {
+          "children": [
+            {
+              "count": "1",
+              "label": "PR Open",
+              "fillColor": "#EF6B6B"
+            },
+            {
+              "count": "0",
+              "label": "PR Merged",
+              "fillColor": "#61CDBB"
+            },
+            {
+              "count": "4",
+              "label": "Commits",
+              "fillColor": "#FAC76E"
+            },
+            {
+              "count": "0",
+              "label": "PR Reviewed",
+              "fillColor": "#C2528B"
+            },
+            {
+              "count": "0",
+              "label": "PR Comments",
+              "fillColor": "#0396A6"
+            },
+            {
+              "count": "0",
+              "label": "Incident Alerts",
+              "fillColor": "#5F50A9"
+            },
+            {
+              "count": "0",
+              "label": "Incidents Resolved",
+              "fillColor": "#8F3519"
+            }
+          ]
+        }
+      },
+      {
+        "date": "2024-05-18",
+        "items": {
+          "children": [
+            {
+              "count": "0",
+              "label": "PR Open",
+              "fillColor": "#EF6B6B"
+            },
+            {
+              "count": "2",
+              "label": "PR Merged",
+              "fillColor": "#61CDBB"
+            },
+            {
+              "count": "1",
+              "label": "Commits",
+              "fillColor": "#FAC76E"
+            },
+            {
+              "count": "1",
+              "label": "PR Reviewed",
+              "fillColor": "#C2528B"
+            },
+            {
+              "count": "1",
+              "label": "PR Comments",
+              "fillColor": "#0396A6"
+            },
+            {
+              "count": "0",
+              "label": "Incident Alerts",
+              "fillColor": "#5F50A9"
+            },
+            {
+              "count": "0",
+              "label": "Incidents Resolved",
+              "fillColor": "#8F3519"
+            }
+          ]
+        }
+      },
+      {
+        "date": "2024-05-19",
+        "items": {
+          "children": [
+            {
+              "count": "2",
+              "label": "PR Open",
+              "fillColor": "#EF6B6B"
+            },
+            {
+              "count": "1",
+              "label": "PR Merged",
+              "fillColor": "#61CDBB"
+            },
+            {
+              "count": "1",
+              "label": "Commits",
+              "fillColor": "#FAC76E"
+            },
+            {
+              "count": "1",
+              "label": "PR Reviewed",
+              "fillColor": "#C2528B"
+            },
+            {
+              "count": "0",
+              "label": "PR Comments",
+              "fillColor": "#0396A6"
+            },
+            {
+              "count": "0",
+              "label": "Incident Alerts",
+              "fillColor": "#5F50A9"
+            },
+            {
+              "count": "1",
+              "label": "Incidents Resolved",
+              "fillColor": "#8F3519"
+            }
+          ]
+        }
+      }
+    ],
+    "activeDays": {
+        "days": 6,
+        "isBurnOut": false,
+        "insight": [
+          "work more dude"
+        ]
+    }
+  }
+  
+  
+
+module.exports = newData;
